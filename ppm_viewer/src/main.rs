f81a76ebbe52f48e9d295ed3816268d25d7bddd9
MaciekTalaska <maciek.talaska@gmail.com>
2019-06-03 19:53:10 +0200

    use shared code for option parsing

--- ppm_viewer/src/main.rs
+++ ppm_viewer/src/main.rs
@@ -1,12 +1,12 @@
 extern crate minifb;
 extern crate tinyppm;
 
-
 use minifb::{Key, Window, WindowOptions};
 use std::env;
 
 fn main() {
-    let image_name = get_image_name();
+    let args: Vec<String> = env::args().collect();
+    let image_name = &shared::get_image_name(&args);
 
     let (width, height, buffer) = tinyppm::ppm_loader::read_image_data(image_name);
 
@@ -18,21 +18,5 @@ fn main() {
     while window.is_open() && !window.is_key_down(Key::Escape) {
         window.update_with_buffer(&buffer).unwrap();
     }
-}
-
-fn get_image_name() -> &String {
-    let args: Vec<String> = env::args().collect();
-    let default_image = &String::from(r"../resources/sky.ppm");
-
-    // make it simple - assume first argument is bitmap name
-    let image_name = match args.len() > 1 {
-        true => args.get(1).unwrap(),
-        false => default_image,
-    };
-    if !image_name.ends_with(".ppm") {
-        println!("only .ppm images are supported");
-        std::process::exit(2);
-    }
-    image_name
 }
 
