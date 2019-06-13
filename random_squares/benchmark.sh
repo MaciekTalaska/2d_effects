84883576f5269c409bad6f843ea5a419372e6a96
MaciekTalaska <maciek.talaska@gmail.com>
2019-06-13 20:27:02 +0200

    final version of benchmark script

--- random_squares/benchmark.sh
+++ random_squares/benchmark.sh
@@ -5,12 +5,11 @@
 touch results.txt
 
 echo -e "fx,\ttile,\ttime" >> results.txt
-#for tilesize in 2 4 5 8 10 16 20 32 40
-for tilesize in 32 40
+for tilesize in 2 4 5 8 10 16 20 32 40
 do
   for FX in 1 2 3 
   do
-    for loops in {1..2}
+    for loops in {1..100}
     do
       ./target/release/random_squares -i ../resources/sky.ppm -t $tilesize -b -f $FX >> results.txt
     done
