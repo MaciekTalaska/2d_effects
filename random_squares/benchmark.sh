2dfe20667fec91bd3e0c91608e3f26fac86f5cd1
MaciekTalaska <maciek.talaska@gmail.com>
2019-06-13 20:17:54 +0200

    improved benchmarking script

--- random_squares/benchmark.sh
+++ random_squares/benchmark.sh
@@ -4,10 +4,15 @@
 
 touch results.txt
 
-echo -e "fx,\ttile size,\ttime" >> results.txt
-./target/release/random_squares -i ../resources/sky.ppm -t 8 -b -f 1 >> results.txt
-
-./target/release/random_squares -i ../resources/sky.ppm -t 8 -b -f 2 >> results.txt
-
-./target/release/random_squares -i ../resources/sky.ppm -t 8 -b -f 3 >> results.txt
-
+echo -e "fx,\ttile,\ttime" >> results.txt
+#for tilesize in 2 4 5 8 10 16 20 32 40
+for tilesize in 32 40
+do
+  for FX in 1 2 3 
+  do
+    for loops in {1..2}
+    do
+      ./target/release/random_squares -i ../resources/sky.ppm -t $tilesize -b -f $FX >> results.txt
+    done
+  done
+done
