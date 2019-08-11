d32d2d6badada7f8cda3d498fb81171f1433498f
MaciekTalaska <maciek.talaska@gmail.com>
2019-08-11 18:59:10 +0200

    make ppm viewer work with latest tinyppm (unreleased yet)

--- ppm_viewer/src/main.rs
+++ ppm_viewer/src/main.rs
@@ -8,15 +8,19 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let image_name = &shared::get_image_name(&args);
 
-    let (width, height, buffer) = tinyppm::ppm_loader::read_image_data(image_name);
+    let ppm_image_result = tinyppm::ppm_loader::read_image_data(image_name);
+    let ppm_image = match ppm_image_result {
+        Ok(image) => image,
+        _ => panic!("unable to read specified image file!"),
+    };
 
     let mut window = Window::new("ppm viewer in Rust",
-                                 width,
-                                 height,
+                                 ppm_image.get_width(),
+                                 ppm_image.get_height(),
                                  WindowOptions::default()).unwrap_or_else(|e| {panic!("{}", e)});
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        window.update_with_buffer(&buffer).unwrap();
+        window.update_with_buffer(&ppm_image.get_pixels()).unwrap();
     }
 }
 
